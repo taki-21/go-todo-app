438bbc65f223c2afc9137dd735d0d015b9272c39
taki-21 <taki.it.0208.21@gmail.com>
2023-07-04 21:20:08 +0900

    log

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"log"
 
+	"github.com/taki-21/go-todo-app/utils"
 	"gopkg.in/go-ini/ini.v1"
 )
 
@@ -17,6 +18,7 @@ var Config ConfigList
 
 func init() {
 	LoadConfig()
+	utils.LoggingSettings(Config.LogFile)
 }
 
 func LoadConfig() {
