fce6da1f8b3be93ac12a2626bf0887965992e68f
taki-21 <taki.it.0208.21@gmail.com>
2023-07-07 00:18:39 +0900

    get todo

--- main.go
+++ main.go
@@ -13,15 +13,15 @@ func main() {
 	// fmt.Println(config.Config.LogFile)
 
 	// log.Println("test")
-	fmt.Println(models.Db)
+	// fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
-	fmt.Println(u)
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "test@example.com"
+	// u.PassWord = "testtest"
+	// fmt.Println(u)
 
-	u.CreateUser()
+	// u.CreateUser()
 
 	// u, _ := models.GetUser(1)
 	// // fmt.Println(u)
@@ -37,6 +37,9 @@ func main() {
 	// u, _ = models.GetUser(1)
 	// fmt.Println(u)
 
-	user, _ := models.GetUser(2)
-	user.CreateTodo("First Todo")
+	// user, _ := models.GetUser(2)
+	// user.CreateTodo("First Todo")
+
+	t, _ := models.GetTodo(1)
+	fmt.Println(t)
 }
