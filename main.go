69dda7c2f3ded4110ede8b6d299bfa4f26918277
taki-21 <taki.it.0208.21@gmail.com>
2023-07-07 00:12:06 +0900

    create todo

--- main.go
+++ main.go
@@ -15,26 +15,28 @@ func main() {
 	// log.Println("test")
 	fmt.Println(models.Db)
 
-	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
-	// u.PassWord = "testtest"
-	// fmt.Println(u)
-
-	// u.CreateUser()
-
-	u, _ := models.GetUser(1)
-	// fmt.Println(u)
-	// fmt.Printf("%T\n", u)
-
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testtest"
 	fmt.Println(u)
 
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	u.CreateUser()
 
+	// u, _ := models.GetUser(1)
+	// // fmt.Println(u)
+	// // fmt.Printf("%T\n", u)
+
+	// u.Name = "Test2"
+	// u.Email = "test2@example.com"
+	// u.UpdateUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
+
+	// u.DeleteUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
+
+	user, _ := models.GetUser(2)
+	user.CreateTodo("First Todo")
 }
