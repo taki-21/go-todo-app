a4297ef0a55543d3ab0793d76bc191685ba83f20
taki-21 <taki.it.0208.21@gmail.com>
2023-07-22 18:32:34 +0900

    add controller

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -22,6 +22,7 @@ func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
 	http.HandleFunc("/", top)
+	http.HandleFunc("/signup", signup)
 
 	// 第二引数をnilにした場合のデフォルトのマルチプレクサは登録されていないURLにアクセスしたら、デフォルトで 404 Page Not Found を返す
 	return http.ListenAndServe(":"+config.Config.Port, nil)
