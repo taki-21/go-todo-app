df085a16e904292bf421d03e81b2fc10438877df
taki-21 <taki.it.0208.21@gmail.com>
2023-07-23 23:11:31 +0900

    add logout function

--- app/models/users.go
+++ app/models/users.go
@@ -107,3 +107,12 @@ func (session *Session) CheckSession() (valid bool, err error) {
 	}
 	return valid, err
 }
+
+func (session *Session) DeleteSessionByUUID() (err error) {
+	cmd := `delete from sessions where uuid = ?`
+	_, err = Db.Exec(cmd, session.UUID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return err
+}
