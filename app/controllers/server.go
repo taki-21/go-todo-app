c78192881762ccaae9b7bcf747979b5478770f84
taki-21 <taki.it.0208.21@gmail.com>
2023-07-24 00:34:02 +0900

    register url

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -38,6 +38,7 @@ func StartMainServer() error {
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/authenticate", authenticate)
 	http.HandleFunc("/todos", index)
+	http.HandleFunc("/todos/new", todoNew)
 
 	// 第二引数をnilにした場合のデフォルトのマルチプレクサは登録されていないURLにアクセスしたら、デフォルトで 404 Page Not Found を返す
 	// 第二引数がnilだから DefaultServeMux が使用される
