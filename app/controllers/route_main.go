6d711c520452ea5b2d9461b79b173aff2af66b78
taki-21 <taki.it.0208.21@gmail.com>
2023-07-26 16:54:18 +0900

    add update & delete function

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"log"
 	"net/http"
+
+	"github.com/taki-21/go-todo-app/app/models"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
@@ -57,6 +59,65 @@ func todoSave(w http.ResponseWriter, r *http.Request){
 			log.Println(err)
 		}
 
+		http.Redirect(w, r, "/todos", 302)
+	}
+}
+
+func todoEdit(w http.ResponseWriter, r *http.Request, id int){
+	session, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		_, err := session.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		t, err := models.GetTodo(id)
+		if err != nil {
+			log.Println(err)
+		}
+		generateHTML(w, t, "layout", "private_navbar", "todo_edit")
+	}
+}
+
+func todoUpdate(w http.ResponseWriter, r *http.Request, id int) {
+	session, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		user, err := session.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		content := r.PostFormValue("content")
+		t := &models.Todo{ID: id, Content: content, UserID: user.ID}
+		if err := t.UpdateTodo(); err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/todos", 302)
+	}
+}
+
+func todoDelete(w http.ResponseWriter, r *http.Request, id int){
+	session, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/logint", 302)
+	} else {
+		_, err := session.GetUserBySession()
+		if err != nil {
+			log.Println(err)
+		}
+		t, err := models.GetTodo(id)
+		if err != nil {
+			log.Println(err)
+		}
+		if err := t.DeleteTodo(); err != nil {
+			log.Println(err)
+		}
 		http.Redirect(w, r, "/todos", 302)
 	}
 }
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/taki-21/go-todo-app/app/models"
 	"github.com/taki-21/go-todo-app/config"
@@ -30,6 +32,26 @@ func session(w http.ResponseWriter, r *http.Request) (session models.Session, er
 	return session, err
 }
 
+var validPath = regexp.MustCompile("^/todos/(edit|update|delete)/([0-9]+)$")
+
+func parseURL(fn func(http.ResponseWriter, *http.Request, int)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// /tods/edit/1
+		q := validPath.FindStringSubmatch(r.URL.Path)
+		if q == nil {
+			http.NotFound(w, r)
+			return
+		}
+		qi, err := strconv.Atoi(q[2])
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+
+		fn(w, r, qi)
+	}
+}
+
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
@@ -41,6 +63,9 @@ func StartMainServer() error {
 	http.HandleFunc("/todos", index)
 	http.HandleFunc("/todos/new", todoNew)
 	http.HandleFunc("/todos/save", todoSave)
+	http.HandleFunc("/todos/edit/", parseURL(todoEdit)) // ハンドラー関数をチェーンさせて指定している？
+	http.HandleFunc("/todos/update/", parseURL(todoUpdate)) // ハンドラー関数をチェーンさせて指定している？
+	http.HandleFunc("/todos/delete/", parseURL(todoDelete)) // ハンドラー関数をチェーンさせて指定している？
 
 	// 第二引数をnilにした場合のデフォルトのマルチプレクサは登録されていないURLにアクセスしたら、デフォルトで 404 Page Not Found を返す
 	// 第二引数がnilだから DefaultServeMux が使用される
